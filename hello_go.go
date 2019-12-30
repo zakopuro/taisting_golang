e21bcd1c4f48b435b0d4262daa9e41317df6ba91
zakopuro <zakopuro@gmail.com>
2019-12-30 16:34:16 +0900

    test

--- hello_go.go
+++ hello_go.go
@@ -1,7 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type Config struct {
+	Name string `json:"name"`
+}
 
 func main() {
-	fmt.Printf("Hello Golang\n")
+	r := gin.Default()
+	r.GET("/", func(c *gin.Context) {
+		c.String(http.StatusOK, "Hello,World!")
+	})
+	r.GET("/user/:name", func(c *gin.Context) {
+		name := c.Param("name")
+		c.String(http.StatusOK, "Hello %s", name)
+	})
+	r.POST("/", func(c *gin.Context) {
+		var samples []Config
+		buf := make([]byte, 2048)
+		n, _ := c.Request.Body.Read(buf)
+		json.Unmarshal(buf[0:n], &samples)
+		c.String(http.StatusOK, "Hello!! %s", samples)
+	})
+	r.Run()
 }
